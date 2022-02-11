71bd2a3960030d2ee6d706427a687dfd6d620240
Sam Armstrong <samuel_e_armstrong@yahoo.co.uk>
2022-02-11 07:33:19 +0000

    Created adaptive softmax layer

---
+++ adaSoft.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class adaSoft(nn.Module):
+    def __init__(self, weight_init = 1) -> None:
+        super(adaSoft, self).__init__()
+        self.softmax = nn.Softmax(dim = -1)
+        initial_scaler = torch.scalar_tensor(weight_init) #torch.Tensor((weight_init))
+        self.scaler = nn.Parameter(data = initial_scaler, requires_grad = True)
+
+    def forward(self, x):
+        x *= self.scaler
+        x = self.softmax(x)
+        return x
